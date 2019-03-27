6940f4f760a300b584a336c91abdb17ea1e033b2
edumoritz <eduardo.moritz@hotmail.com>
2019-03-27 19:45:20 -0300

    problema de monty hall #02 && #03

--- vue/montyhall/src/App.vue
+++ vue/montyhall/src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <div id="app">
     <h1>Problema de Monty Hall</h1>
+    <Door number="1" :hasGift="false" />
   </div>
 </template>
 
 <script>
+import Door from './components/Door'
 export default {
-
+  name: 'App',
+  components: { Door }
 }
 </script>
 
 <style>
 * {
   box-sizing: border-box;
-  font-family: 'Montserrat', sans-serif/
+  font-family: 'Montserrat', sans-serif;
 }
 
 body {
