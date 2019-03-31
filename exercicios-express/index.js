46dfd64d9db678fdb4fc8459372a77a48ebdd05f
edumoritz <eduardo.moritz@hotmail.com>
2019-03-31 13:10:47 -0300

    middlewares

--- exercicios-express/index.js
+++ exercicios-express/index.js
@@ -1,5 +1,33 @@
 const express = require('express')
 const app = express()
+const saudacao = require('./saudacaoMid')
+
+app.use(saudacao('Eduardo'))
+
+app.use((req, res, next) => {
+  console.log('Antes...')
+  next()
+})
+
+app.get('/opa', (req, res, next) => {
+  console.log('Durante...')
+  res.json({
+    data: [
+      {id: 7, name: 'Ana', position: 1},
+      {id: 34, name: 'Bia', position: 2},
+      {id: 73, name: 'Carlos', position: 3},
+    ],
+    count: 30,
+    skip: 0,
+    limit: 3,
+    status: 200
+  })
+  next()
+})
+
+app.use((req, res, next) => {
+  console.log('Depois...')
+})
 
 app.listen(3000, () => {
   console.log('Backend executando!')
