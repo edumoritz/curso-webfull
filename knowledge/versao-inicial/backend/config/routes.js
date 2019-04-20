671bbf3fa86b82c6e7146dbd22b5841223e10176
edumoritz <eduardo.moritz@hotmail.com>
2019-04-20 11:44:06 -0300

    artigos

--- knowledge/versao-inicial/backend/config/routes.js
+++ knowledge/versao-inicial/backend/config/routes.js
@@ -18,5 +18,18 @@ module.exports = app => {
   app.route('/categories/:id')
     .get(app.api.category.getById)
     .put(app.api.category.save)       
-    .delete(app.api.category.remove)       
+    .delete(app.api.category.remove)     
+    
+  app.route('/articles')
+    .get(app.api.article.get)
+    .post(app.api.article.save) 
+    
+  app.route('/articles/:id')
+    .get(app.api.article.getById)
+    .put(app.api.article.save) 
+    .delete(app.api.article.remove)  
+
+  app.route('/categories/:id/articles')
+    .get(app.api.article.getByCategory)
+
 }
