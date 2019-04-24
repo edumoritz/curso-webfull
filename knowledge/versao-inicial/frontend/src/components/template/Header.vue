03f7e03404c37201b13b7c0d5ec8d52999408183
edumoritz <eduardo.moritz@hotmail.com>
2019-04-24 19:36:57 -0300

    Componente Menu do Usuário

--- knowledge/versao-inicial/frontend/src/components/template/Header.vue
+++ knowledge/versao-inicial/frontend/src/components/template/Header.vue
@@ -6,12 +6,16 @@
       <h1 class="title">
         {{ title }}
       </h1>
+      <UserDropdown v-if="!hideUserDropdown" />
   </header>
 </template>
 
 <script>
+import UserDropdown from './UserDropDown'
+
 export default {
   name: 'Header',
+  components: { UserDropdown },
   props: {
     title: String, 
     hideToggle: Boolean  
@@ -61,6 +65,7 @@ export default {
     align-items: center;
   }
   header.header > a.toggle:hover {
+    color: #fff;
     background-color: rgba(0, 0, 0, 0.2);
   }
 </style>
