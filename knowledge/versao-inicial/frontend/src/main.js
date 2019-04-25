ba36f87855a30752c169250132e856f9e0426067
edumoritz <eduardo.moritz@hotmail.com>
2019-04-25 18:37:56 -0300

    Páginas Administrativas

--- knowledge/versao-inicial/frontend/src/main.js
+++ knowledge/versao-inicial/frontend/src/main.js
@@ -10,6 +10,9 @@ import router from './config/router'
 
 Vue.config.productionTip = false
 
+// TEMPORARIO!
+require('axios').defaults.headers.common['Authorization'] = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IkVkdWFyZG8gTW9yaXR6IiwiZW1haWwiOiJlZHVhcmRvLm1vcml0ekBob3RtYWlsLmNvbSIsImFkbWluIjp0cnVlLCJpYXQiOjE1NTYyMjY4MjEsImV4cCI6MTU1NjQ4NjAyMX0.5dfsp2g-kLVpxrU1JAu7947xc1RNAB6f6RyQ6fJGVoI'
+
 new Vue({
   store,
   router,
