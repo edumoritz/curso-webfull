f43a727713b1dc741595fa4b26c998a4331f2dc1
edumoritz <eduardo.moritz@hotmail.com>
2019-05-02 02:09:00 -0300

    cadastro de artigos #1

--- knowledge/versao-inicial/frontend/src/components/admin/CategoryAdmin.vue
+++ knowledge/versao-inicial/frontend/src/components/admin/CategoryAdmin.vue
@@ -2,19 +2,17 @@
   <div class="category-admin">
 
     <b-form>
-      
+
       <input id="category-id" type="hidden" v-model="category.id" />
 
       <b-form-group label="Nome:" label-for="category-name">
         <b-form-input id="category-name" type="text" v-model="category.name" required 
           placeholder="Informe o Nome da Categoria..." :readonly="mode === 'remove'" />
       </b-form-group>
-
     
       <b-form-group label="Categoria Pai:" label-for="category-parentId">
         <b-form-select v-if="mode === 'save'"
           id="category-parentId"
-          :readonly="mode === 'remove'"
           :options="categories" v-model="category.parentId" />
         <b-form-input v-else
           id="category-parentId" type="text" 
